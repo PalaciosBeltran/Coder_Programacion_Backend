2192f3fcced71e2a8c3fc721b7a69cc9830d9420
Eduardo Palacios <111165643+PalaciosBeltran@users.noreply.github.com>
2022-09-20 23:25:23 -0500

    Add files via upload

---
+++ routers/products/products.routes.js
@@ -0,0 +1,38 @@
+const express = require(`express`);
+const fs = require('fs');
+const Products = require('../../model/products/products.api');
+
+const router = express.Router();
+
+// Middlewares
+router.use(express.json());
+
+let importedProducts = new Products();
+
+// Routes
+router.get('/', async (req, res) => {
+    const read = await importedProducts.read(req, res);
+    res.end();
+});
+
+router.get('/:id', async (req, res) => {
+  const productByID = await importedProducts.getById(req, res);
+  res.end();
+});
+  
+router.post('/', async (req, res) => {
+  const productByID = await importedProducts.save(req, res);
+  res.end();
+});
+  
+router.put('/:id', async (req, res) => {
+  const productByID = await importedProducts.change(req, res);
+  res.end();
+});
+  
+router.delete('/:id', async (req, res) => {
+  const productByID = await importedProducts.deleteById(req, res);
+  res.end();
+});
+
+module.exports = router;
