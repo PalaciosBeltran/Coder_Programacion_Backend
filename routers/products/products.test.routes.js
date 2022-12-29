fe8e9afc897403517bd9f496060083319c3189a7
Eduardo Palacios <111165643+PalaciosBeltran@users.noreply.github.com>
2022-12-29 12:29:15 -0500

    Delete routers directory

--- routers/products/products.test.routes.js
+++
@@ -1,8 +0,0 @@
-// const express = require('express');
-// const productsController = require('../../controllers/products.controllers');
-
-// const router = express.Router();
-
-// router.get('/', productsController.getFakeProducts);
-
-// module.exports = router;
